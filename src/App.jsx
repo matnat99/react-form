ad519d5dd247115d5880f0cf2f5e1bc1d2ada9b2
Mattia Natale <mattianatale07@gmail.com>
2025-02-03 17:14:09 +0100

    feat: add milestone 2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useState } from "react";
 
 export default function App() {
   const [shoppingList, setShoppingList] = useState(["Pane", "Latte", "Uova"]);
+  const [productName, setProductName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    setShoppingList([...shoppingList, productName]);
+
+    setProductName("");
+  };
 
   return (
     <div>
@@ -11,6 +20,19 @@ export default function App() {
           <li key={index}>{product}</li>
         ))}
       </ul>
+      <hr />
+      <h2>Aggiungi prodotto</h2>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={productName}
+          onChange={(event) => {
+            setProductName(event.target.value);
+          }}
+          placeholder="Inserisci prodotto"
+        />
+        <button type="submit">Invia</button>
+      </form>
     </div>
   );
 }
